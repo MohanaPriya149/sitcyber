0bed2615eb96c4047e28c1c19068a3d2838d3e03
MohanaPriya149 <mohanapriya140904@gmail.com>
2025-06-20 14:09:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// Disable keyboard and mouse
-function disableInput() {
-  document.addEventListener('keydown', e => e.preventDefault(), true);
-  document.addEventListener('keyup', e => e.preventDefault(), true);
-  document.addEventListener('keypress', e => e.preventDefault(), true);
-  document.addEventListener('mousemove', e => e.stopPropagation(), true);
-  document.addEventListener('mousedown', e => e.stopPropagation(), true);
-  document.addEventListener('mouseup', e => e.stopPropagation(), true);
-  document.addEventListener('click', e => e.stopPropagation(), true);
-}
 
-disableInput();
-
-// Re-enable after 60 seconds by reloading page
-setTimeout(() => {
-  location.reload();
-}, 60000);
